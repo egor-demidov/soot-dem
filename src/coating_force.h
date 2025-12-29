e8713f883f264b0554a7c13819b4b0cb07116956
Egor Demidov <mail@edemidov.com>
2025-12-29 17:35:02 -0500

    Backup - adding a three-body capillary force model

--- src/coating_force.h
+++ src/coating_force.h
@@ -5,6 +5,131 @@
 #ifndef SOOT_AFM_COATING_FORCE_H
 #define SOOT_AFM_COATING_FORCE_H
 
+#include <list>
+
+// A filling-angle based coating model
+template <typename field_value_t, typename real_t>
+struct filling_angle_coating_functor {
+    filling_angle_coating_functor(
+        real_t r_part,                          // Primary particle radius
+        real_t d_crit,                          // Critical separation between particles to consider them neighbors
+        real_t magnitude,                       // Magnitude of the attractive force (related to surface tension)
+        real_t filling_angle,                   // Filling angle (corresponds to coating volume)
+        real_t drop_rate,                       // Smoothness of the sigmoid function
+        real_t mass,                            // Mass of primary particle
+        field_value_t field_zero,               // Zero vector
+        std::vector<field_value_t> const & x0   // Initial positions of particles
+    )
+        : r_part{r_part}
+        , d_crit{d_crit}
+        , magnitude{magnitude}
+        , filling_angle{filling_angle}
+        , drop_rate{drop_rate}
+        , mass {mass}
+        , field_zero{field_zero}
+    {
+        // Initialize angles
+        long n_particles = x0.size();
+        angle_lists.resize(n_particles);
+
+        // Iterate over triplets
+        for (long i = 0; i < n_particles - 2; i ++) {
+            for (long j = i + 1; j < n_particles - 1; j ++) {
+                for (long k = j + 1; k < n_particles; k ++) {
+
+                    real_t d_ij = (x0[i] - x0[j]).norm() - 2.0 * r_part;
+                    real_t d_ik = (x0[i] - x0[k]).norm() - 2.0 * r_part;
+                    real_t d_jk = (x0[j] - x0[k]).norm() - 2.0 * r_part;
+
+                    if (d_ij < d_crit && d_ik < d_crit) {
+                        // Create angle jik...
+                        field_value_t ij = (x0[j] - x0[i]).normalized();
+                        field_value_t ik = (x0[k] - x0[i]).normalized();
+                        real_t alpha = acos(ij.dot(ik));
+                        angles.push_back(j, i, k, alpha);
+
+                        // Add reference to this angle to non-central particles
+                        angle_lists[j] = angles.back();
+                        angle_lists[k] = angles.back();
+                    }
+
+                    if (d_ij < d_crit && d_jk < d_crit) {
+                        // Create angle ijk...
+                        field_value_t ji = (x0[i] - x0[j]).normalized();
+                        field_value_t jk = (x0[k] - x0[j]).normalized();
+                        real_t alpha = acos(ji.dot(jk));
+                        angles.push_back(i, j, k, alpha);
+
+                        // Add reference to this angle to non-central particles
+                        angle_lists[i] = angles.back();
+                        angle_lists[k] = angles.back();
+                    }
+
+                    if (d_ik < d_crit && d_jk < d_crit) {
+                        // Create angle ikj...
+                        field_value_t ki = (x0[i] - x0[k]).normalized();
+                        field_value_t kj = (x0[j] - x0[k]).normalized();
+                        real_t alpha = acos(ki.dot(kj));
+                        angles.push_back(i, k, j, alpha);
+
+                        // Add reference to this angle to non-central particles
+                        angle_lists[i] = angles.back();
+                        angle_lists[j] = angles.back();
+                    }
+
+                }
+            }
+        }
+    }
+
+    // TODO: add function update_angles()
+
+    void update_angles(std::vector<field_value_t> const & x) {
+        // WARNING: currently new angles are not detected, only angle values are recomputed
+
+        for (auto & ang : angles) {
+            field_value_t ji = (x[ang.i] - x[ang.j]).normalized();
+            field_value_t jk = (x[ang.k] - x[ang.j]).normalized();
+            real_t alpha = acos(ji.dot(jk));
+        }
+    }
+
+    std::pair<field_value_t, field_value_t> operator () (size_t i,
+            std::vector<field_value_t> const & x,
+             std::vector<field_value_t> const & v [[maybe_unused]],
+             std::vector<field_value_t> const & theta [[maybe_unused]],
+             std::vector<field_value_t> const & omega [[maybe_unused]],
+             real_t t [[maybe_unused]]) const {
+
+        field_value_t f = field_zero;
+        for (auto const & ang_itr : angle_lists[i]) {
+            long j;
+            if (i != ang_itr->i)
+                j = ang_itr->i;
+            else
+                j = ang_itr->k;
+
+            field_value_t n = (x[j] - x[i]).normalized();
+            f += n * 0.5 * magnitude * (1.0 - tanh(drop_rate * (ang_itr->alpha - 2.0 * filling_angle)));
+        }
+
+        return std::make_pair(f / mass, 0.0);
+    }
+
+private:
+
+    struct angle {
+        long i, j, k;   // j is the central particle
+        real_t alpha;   // angle (in radians)
+    };
+
+    std::list<angle> angles;
+    std::vector<typename std::list<angle>::iterator> angle_lists;
+
+    const real_t r_part, d_crit, magnitude, filling_angle, drop_rate, mass;
+    const field_value_t field_zero;
+};
+
 template <typename field_value_t, typename real_t>
 struct binary_coating_functor {
     binary_coating_functor(real_t d_cutoff, real_t magnitude, real_t drop_rate, real_t mass, field_value_t field_zero) :
